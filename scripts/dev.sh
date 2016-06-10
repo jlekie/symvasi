79d2508dfa90d059754ca4951ce40f3b11deae37
Joe Lekie <joe.lekie@gmail.com>
2016-06-10 15:56:25 -0500

    Minor build tweaks

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -3,5 +3,9 @@
 if [ $# -eq 0 ]; then
     docker run -ti --rm --net=host --privileged -v $(pwd):/ws jlekie/devenv-nodejs:5.x $USER `id -u $USER` `id -g $USER`
 else
-    docker run --rm --net=host --privileged -v $(pwd):/ws jlekie/devenv-nodejs:5.x $USER `id -u $USER` `id -g $USER` $@
+    if [ -t 1 ]; then
+        docker run -ti --rm --net=host --privileged -v $(pwd):/ws jlekie/devenv-nodejs:5.x $USER `id -u $USER` `id -g $USER` $@
+    else
+        docker run -t --rm --net=host --privileged -v $(pwd):/ws jlekie/devenv-nodejs:5.x $USER `id -u $USER` `id -g $USER` $@
+    fi
 fi
