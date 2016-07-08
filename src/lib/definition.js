02a728da886b476be4f3a73965120f45c4d46270
Joe Lekie <joe.lekie@gmail.com>
2016-07-08 11:56:15 -0500

    Additional definition refactoring Added support for inherited extension properties

--- src/lib/definition.js
+++ src/lib/definition.js
@@ -24,7 +24,18 @@ export default class Definition {
     get name() { return priv(this).name; }
 
     resolveContext(context) {
-        return _.defaults({ name: this.name, context: context }, this);
+        return {
+            name: this.name,
+            options: this.options,
+            enums: this.enums.map(e => e.resolveContext()),
+            contracts: this.contracts.map(e => e.resolveContext()),
+            models: this.models.map(e => e.resolveContext()),
+            services: this.services.map(e => e.resolveContext()),
+
+            extensions: _.assign({}, this.extensions),
+
+            context: context
+        };
     }
 }
 
@@ -36,14 +47,32 @@ class Enum {
         this.values = props.values ? _.clone(props.values) : {};
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
     }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            values: _.clone(this.values),
+
+            extensions: _.assign({}, this.manifest.extensions, this.extensions)
+        };
+    }
 }
 class Contract {
     constructor(manifest, props) {
         this.manifest = manifest;
 
         this.name = props.name;
-        this.properties = _.map(props.properties || [], props => new Property(this, props));
+        this.properties = _.map(props.properties || [], props => new ContractProperty(this, props));
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
+    }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            properties: this.properties.map(e => e.resolveContext()),
+            
+            extensions: _.assign({}, this.manifest.extensions, this.extensions)
+        };
     }
 }
 class Model {
@@ -51,7 +80,7 @@ class Model {
         this.manifest = manifest;
         
         this.name = props.name;
-        this.properties = _.map(props.properties || [], props => new Property(this, props));
+        this.properties = _.map(props.properties || [], props => new ModelProperty(this, props));
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
         this.contracts = props.contracts ? _.clone(props.contracts) : {};
 
@@ -60,7 +89,7 @@ class Model {
             for (let prop of contract.properties) {
                 let existingProp = _.find(this.properties, existingProp => existingProp.name === prop.name);
                 if (!existingProp) {
-                    this.properties.push(new Property(manifest, prop));
+                    this.properties.push(new ModelProperty(this, prop));
                 }
                 else {
                     if (existingProp.type !== prop.type) {
@@ -79,34 +108,91 @@ class Model {
             return contract;
         });
     }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            contracts: _.clone(this.contracts),
+            properties: this.properties.map(e => e.resolveContext()),
+            
+            extensions: _.assign({}, this.manifest.extensions, this.extensions)
+        };
+    }
 }
 class Service {
     constructor(manifest, props) {
         this.manifest = manifest;
         
         this.name = props.name;
-        this.methods = _.map(props.methods || [], props => new Method(this, props));
+        this.methods = _.map(props.methods || [], props => new ServiceMethod(this, props));
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
+    }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            methods: this.methods.map(e => e.resolveContext()),
+            
+            extensions: _.assign({}, this.manifest.extensions, this.extensions)
+        };
     }
 }
 
-class Property {
-    constructor(manifest, props) {
-        this.manifest = manifest;
+class ContractProperty {
+    constructor(contract, props) {
+        this.contract = contract;
 
         this.name = props.name;
         this.type = props.type;
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
     }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            type: this.type,
+            
+            extensions: _.assign({}, this.contract.manifest.extensions, this.contract.extensions, this.extensions)
+        };
+    }
 }
-class Method {
-    constructor(manifest, props) {
-        this.manifest = manifest;
+class ModelProperty {
+    constructor(model, props) {
+        this.model = model;
+
+        this.name = props.name;
+        this.type = props.type;
+        this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
+    }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            type: this.type,
+            
+            extensions: _.assign({}, this.model.manifest.extensions, this.model.extensions, this.extensions)
+        };
+    }
+}
+
+class ServiceMethod {
+    constructor(service, props) {
+        this.service = service;
 
         this.name = props.name;
         this.returnType = props.returnType;
         this.params = _.map(props.params || [], props => new MethodParam(this, props));
         this.extensions = props.extensions ? _.cloneDeep(props.extensions) : {};
+    }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            returnType: this.returnType,
+            params: this.params.map(e => e.resolveContext()),
+            
+            extensions: _.assign({}, this.service.manifest.extensions, this.service.extensions, this.extensions)
+        };
     }
 }
 class MethodParam {
@@ -115,5 +201,12 @@ class MethodParam {
         
         this.name = props.name;
         this.type = props.type;
+    }
+
+    resolveContext() {
+        return {
+            name: this.name,
+            type: this.type
+        };
     }
 }
